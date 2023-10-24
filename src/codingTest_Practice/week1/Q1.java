58eea33f23477328765da6bce786bcedb0bb0e37
user <user@DESKTOP-P99H1R2>
2023-10-24 13:35:12 +0900

    init

---
+++ src/codingTest_Practice/week1/Q1.java
@@ -0,0 +1,18 @@
+package codingTest_Practice.week1;
+
+public class Q1 {
+
+	public static void main(String[] args) {
+
+		int num=68 ;
+		int n =2;
+
+		if (num % n == 0 ) {
+			System.out.println(1);
+			
+		}else {
+			System.out.println(0);
+		}
+
+	}
+}
